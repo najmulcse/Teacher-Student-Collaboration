70fb9d9bc9e5e8ff047a8ca087a6c3eef598adb8
Najmul <najmul2022@gmail.com>
2017-04-10 00:29:27 +0600

    Student & Teachers tables are inserted to git hub

---
+++ database/migrations/2017_04_09_173117_create_students_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateStudentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('students', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id');
+            $table->integer('roll');
+            $table->string('year');
+            $table->string('semester');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('students');
+    }
+}
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,10 +4,10 @@
     <title> TS | Home</title>
 @endsection
 @section('content')
-<div class="container">
+<div class="container-fluid">
     <div class="row">
         <div class="col-sm-3">
-
+        <h1>Hello home</h1>
         </div>
         <div class="col-sm-8">
 

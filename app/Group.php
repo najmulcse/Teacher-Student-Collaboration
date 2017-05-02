3b8784dcd272e9cce53381796a4cabf92a5e23af
Najmul <najmul2022@gmail.com>
2017-05-02 15:46:59 +0600

    User and group model is linked sucessfully

--- app/Group.php
+++ app/Group.php
@@ -6,7 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Group extends Model
 {
-    
+    protected $guarded=[''];
+
+
     public function user(){
 
     return $this->belongsTo('App\User');

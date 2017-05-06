cd2725f65acac66a87d749930a793e9f2904d02e
Najmul <najmul2022@gmail.com>
2017-05-06 20:38:43 +0600

    Added links in  the group options

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -47,10 +47,10 @@
                                                   </a>
 
                                                   <ul class="dropdown-menu" role="menu">
-                                                      <li><a href="{{ route('group_id',['id'=>$group->id]) }}"><i class="fa fa-pencil fa-fw"></i>edit</a></li>
+                                                      <li><a href="{{ route('group_id',['id'=>$group->id]) }}"><i class="fa fa-pencil fa-fw"></i>Edit</a></li>
 
-                                                     <li><a onclick="return confirm('are you sure?')" href="{{ route('group_deleted_id',['id'=>$group->id]) }}"><i class="fa fa-trash-o fa-fw"></i>delete</a></li>
-                                                      <li><button class="btn btn-sm"><span class=""><i class="fa fa-cog fa-fw"></i></span>Add member</button></li>
+                                                     <li><a onclick="return confirm('are you sure?')" href="{{ route('group_deleted_id',['id'=>$group->id]) }}"><i class="fa fa-trash-o fa-fw"></i>Delete</a></li>
+                                                      <li><a href="#"><span class=""><i class="fa fa-plus fa-fw"></i></span>Add member</a></li>
 
                                                       
 
